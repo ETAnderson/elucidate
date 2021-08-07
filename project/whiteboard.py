341e036eb246b8ce922d4d3b4dda265c3fd60364
Eric Anderson <anderson.eric.t@gmail.com>
2021-08-07 18:02:31 -0400

    basic api interactions created: next step, lift up functions into object

--- project/whiteboard.py
+++ project/whiteboard.py
@@ -1,28 +1,36 @@
 import requests
-import json
-
+# import json
+# replace with model
 sentence = 'what a big house'
+known_words = ['what', 'a']
 
 
 def get_definition(word):
+    word = str(word)
     word = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en_US/{word}')
     word_python_object = word.json()
     definition = dict(d.popitem() for d in word_python_object)
 
-    print(json.dumps(definition, indent=4))
-
     return definition
 
 
+def remove_known_words(sentence):
+    sentence = sentence.split()
+    sentence = [x for x in sentence if x not in known_words]
+
+    return sentence
+
+
 def get_sentence_of_definitions(sentence):
-    words = sentence.split()
     word_definitions = []
 
-    for word in words:
-        word_definition = get_definition(word)
-        sentence_of_word_definitions = word_definitions.append(word_definition)
+    for word in sentence:
+        definition = get_definition(word)
+        word_definitions.append(definition)
+        newworddef = print(word)
+        newworddef = print(word_definitions)
 
-    return sentence_of_word_definitions
+    return word_definitions
 
 # print tests for get_definition function
 # word_def = get_definition(word)
@@ -31,6 +39,10 @@ def get_sentence_of_definitions(sentence):
 # print(word_def["meanings"][1]['partOfSpeech'])
 
 
+# print test for remove_known_words function
+print('start :' + sentence)
+new_sentence = remove_known_words(sentence)
+print(new_sentence)
 # print tests for get_sentence_of_definitions function
-sentence_definitions = get_sentence_of_definitions(sentence)
+sentence_definitions = get_sentence_of_definitions(new_sentence)
 print(sentence_definitions)
